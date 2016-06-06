725fe0e03cfb4df40e4207c5fd7073530cd0c9dc
bikashsapkota <bikash.sapkota@deerwalk.edu.np>
2016-06-06 18:50:41 +0545

    Update Bikash_0338_lab6.groovy
    Lab 6 completed

--- 0338_Bikash/Bikash_0338_lab6.groovy
+++ 0338_Bikash/Bikash_0338_lab6.groovy
@@ -1,9 +1,9 @@
-//this lab is complete only upto task 3. It will be complete by 1st july 2016 i.e after final exam
 def fileContents = new File('/home/bikash/Desktop/shakespeare.txt').getText();
 def lists = fileContents.tokenize(' !.&,;?\n:');
 def size = lists.size()
 def count = 0;
 def words = [];
+def probablemap = [:];
 def map = [:];
 def maps = [:];
 def temp = "";
@@ -26,9 +26,11 @@ for(int i=0; i<size-1; i++){
         maps.put(temp,1)  
     }else{
         maps.put(temp,maps[temp]+1)
+       
     }
+   
 }
-count = words.size()
+count = lists.size()
 //calculate the probability of given word 
 def probabiliti(word,map,count){
     word = "$word"
@@ -74,6 +76,10 @@ println "p(will, look, upon)="+prob('will','look','upon',maps,map,count)
 println "p(i, am, no, baby)="+prob('i','am','no','baby',maps,map,count)
 println "p(wherefore, art, thou, Romeo)="+prob('wherefore','art','thou','Romeo',maps,map,count)
 
+println "p(have, sent) indepently="+probabiliti('have',map,count)*probabiliti('sent',map,count)
+println "p(will, look, upon) indepently="+probabiliti('will',map,count)*probabiliti('look',map,count)*probabiliti('upon',map,count)
+println "p(i, am, no, baby) indepently="+probabiliti('i',map,count)*probabiliti('am',map,count)*probabiliti('no',map,count)*probabiliti('baby',map,count)
+println "p(wherefore, art, thou, Romeo) indepently="+probabiliti('wherefore',map,count)*probabiliti('art',map,count)*probabiliti('thou',map,count)*probabiliti('romeo',map,count)
 
 def sort = map.sort { b, a -> a.value <=> b.value }
 def sorts = maps.sort { b, a -> a.value <=> b.value }
@@ -84,7 +90,7 @@ i=0
 sort.each{
     if(!neglet.contains(it.key)) {
         i++
-        println  i+"     "+it.key+"      "+it.value
+        //println  i+"     "+it.key+"      "+it.value
     }
 }
 
@@ -92,6 +98,36 @@ sort.each{
 i=0
 println "Rank  Wordpair    Frequency"
 sorts.each {
-    println i + "     " + it.key + "      " + it.value
+    //println i + "     " + it.key + "      " + it.value
     i++
+}
+
+def prediction(word1, word2, word3,lists) {
+    def probablemap = [:];
+    count = lists.size()
+    for(int i=0; i<count-3; i++){
+        if(lists[i]==word1 && lists[i+1]==word2 && lists[i+2]==word3){
+            if(probablemap[lists[i+3]]==null)
+                probablemap.put(lists[i+3],1)
+            else
+                probablemap.put(lists[i+3],probablemap[lists[i+3]]+1)
+
+        }
+    }
+    return probablemap
+}
+
+probablemap = prediction("i","am","no",lists)
+def probablemap1 = prediction("wherefore","art","thou",lists)
+probablemap = probablemap.sort { b, a -> a.value <=> b.value }
+probablemap1 = probablemap1.sort { b, a -> a.value <=> b.value }
+
+print "The Most Probable word after I am no are: "
+probablemap.each{
+    println it.key
+}
+
+print "The Most Probable word after wherefore art thou is: "
+probablemap1.each{
+    print it.key+" "
 }
