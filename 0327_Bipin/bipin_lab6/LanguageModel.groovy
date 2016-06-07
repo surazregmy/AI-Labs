722979b7863a39b87b532146f8a0ea4c336c4165
BipinPaudel <paudelbipin.bp@gmail.com>
2016-06-08 00:16:46 +0545

    assignment added

--- 0327_Bipin/bipin_lab6/LanguageModel.groovy
+++ 0327_Bipin/bipin_lab6/LanguageModel.groovy
@@ -3,47 +3,8 @@ package bipin_lab6
  * Created by bips on 4/8/16.
  */
 class LanguageModel {
-    private static def wordList=[]
-    private static def wordMap=[:]
-//this function calculates the probability for the bigram model
-    public static takeData(wordForProb) {
-        def probability=0
-        switch(wordForProb.size()){
-            case 2: // if it is bi-gram model
-                probability=calculateProbability(wordForProb)
-                break
-            case 3: // if it is tri-gram model
-                probability=  wordMap.get(wordForProb[0])/wordList.size()  * calculateProbability([wordForProb[0],wordForProb[1]])*
-                        calculateProbability([wordForProb[1],wordForProb[2]])
-                break
-            case 4: // if it is 4-gram model
-                probability=wordMap.get(wordForProb[0])/wordList.size()  * calculateProbability([wordForProb[0],wordForProb[1]])*
-                        calculateProbability([wordForProb[1],wordForProb[2]],) *
-                        calculateProbability([wordForProb[2],wordForProb[3]])
-                break
-
-        }
-        println(probability)
-
-
-    }
-
-    public static float calculateProbability(wordForProb){//takes two words in a list and find the probability
-        def counter = 0
-        def probability
-        for (int i = 0; i < wordList.size - 2; i++) {
-            def firstWord = wordList[i]
-            def secondWord = wordList[i + 1]
-            if (firstWord == wordForProb[0] && secondWord == wordForProb[1]) {
-                counter++
-            }
-        }
-        probability = counter / (wordMap.get(wordForProb[0]))
-       return probability
-    }
-
-    //
-
+    private static def wordList = []
+    private static def wordMap = [:]
 
     public static void main(String[] args) {
         String fileContents;
@@ -64,16 +25,98 @@ class LanguageModel {
                 wordMap[it.toLowerCase()] = 1
             }
         }
-        //sample word for which we want to calculate the probability of
-        //this is a bigram model
-        def wordForProb = ["the", "court"] //this is the list of words we want to calculate probability of
-        takeData(wordForProb)
+        wordMap = wordMap.sort { -it.value }
+        def frequent = []
+        def counter = 1
+        wordMap.each {
+            if (counter == 21) {
+                return true
+            }
+            def temp = []
+            temp.add(counter)
+            temp.add(it.getKey())
+            temp.add(it.getValue())
+            frequent.add(temp)
+            counter++
+            return false
+        }
+        println("Top 20 frequent words with rank, word and frequency respectively: ")
+        println(frequent)
 
-        takeData(["love", "is"])
-        takeData(["is","my"])
-        println("3 trigram")
-        takeData(["he","is","my"])
-        println("4 gram")
-        takeData(["the","court","is","good"])
+
+
+        println("Probability of words")
+
+        println("Probability of \"the\" " + takeData(["the"]))
+        println("Probability of \"become\" " + takeData(["become"]))
+        println("Probability of \"brave \" " + takeData(["brave"]))
+        println("Probability of \"treason\" " + takeData(["treason"]))
+
+        println("--------------------\n")
+        println("2 ")
+        println("P(court|the) " + takeData(["the", "court"]))
+        println("P(word|his) " + takeData(["his", "word"]))
+        println("P(qualities|rare) " + takeData(["rare", "qualities"]))
+        println("P(men|young) " + takeData(["young", "men"]))
+
+        println("------------------\n")
+        println("Chain rule- dependent case")
+        println("Probability of P(have, sent) " + takeData(["have", "sent"]))
+        println("Probability of P(will, look, upon)  " + takeData(["will", "look", "upon"]))
+        println("Probability of P(i, am, no, baby) " + takeData(["i", "am", "no", "baby"]))
+        println("Probability of P(wherefore, art, thou, romeo) " + takeData(["wherefore", "art", "thou", "romeo"]))
+
+        println("------------------------\n")
+        println("Chain rule- independent case")
+        println("Probability of P(have, sent) " + independentProbability(["have", "sent"]))
+        println("Probability of P(will, look, upon)  " + independentProbability(["will", "look", "upon"]))
+        println("Probability of P(i, am, no, baby) " + independentProbability(["i", "am", "no", "baby"]))
+        println("Probability of P(wherefore, art, thou, romeo) " + independentProbability(["wherefore", "art", "thou", "romeo"]))
+    }
+
+//this function calculates the probability for the bigram model
+    public static float takeData(wordForProb) {
+        def probability = 0
+        switch (wordForProb.size()) {
+            case 1:
+                probability = wordMap.get(wordForProb[0]) / wordList.size()
+                break
+            case 2: // if it is bi-gram model
+                probability = calculateProbability(wordForProb)
+                break
+            case 3: // if it is tri-gram model
+                probability = wordMap.get(wordForProb[0]) / wordList.size() * calculateProbability([wordForProb[0], wordForProb[1]]) *
+                        calculateProbability([wordForProb[1], wordForProb[2]])
+                break
+            case 4: // if it is 4-gram model
+                probability = wordMap.get(wordForProb[0]) / wordList.size() * calculateProbability([wordForProb[0], wordForProb[1]]) *
+                        calculateProbability([wordForProb[1], wordForProb[2]],) *
+                        calculateProbability([wordForProb[2], wordForProb[3]])
+                break
+        }
+        return probability
+    }
+
+    public static float calculateProbability(wordForProb) {//takes two words in a list and find the probability
+        def counter = 0
+        def probability
+        for (int i = 0; i < wordList.size - 2; i++) {
+            def firstWord = wordList[i]
+            def secondWord = wordList[i + 1]
+            if (firstWord == wordForProb[0] && secondWord == wordForProb[1]) {
+                counter++
+            }
+        }
+        probability = counter / (wordMap.get(wordForProb[0]))
+        return probability
+    }
+
+
+    public static float independentProbability(wordList) {
+        def answer = 1
+        wordList.each {
+            answer *= takeData([it])
+        }
+        return answer
     }
 }
